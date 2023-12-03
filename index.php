fc5f4fbd16541062d9b1b33ef010f825894536b5
ChuthamatT <120088132+ChuthamatT@users.noreply.github.com>
2023-12-04 00:05:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ระบบยืม - คืน อุปกรณ์กีฬามหาวิทยาลัยธนบุรี </title>
+<title>ระบบยืม - คืน อุปกรณ์กีฬา</title>
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <script language="javascript">
 function chk_login() {
